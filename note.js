704885812519df5c4bb93c504eb73a836068df44
Massonn.js <68811288+masson-js@users.noreply.github.com>
2022-08-27 21:57:30 +0200

    delete

--- note.js
+++
@@ -1,9 +0,0 @@
-
-
-const para = document.createElement("p");
-const node = document.createTextNode("This is a new paragraph.");
-
-para.appendChild(node);
-const element = document.getElementById("main-div");
-element.appendChild(para);
-
